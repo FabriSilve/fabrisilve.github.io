5c2047bd793cb71d94fea1432d85263400c42f59
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2024-02-22 22:09:15 +0100

    make scrolling smooth

--- app/router.options.ts
+++ app/router.options.ts
@@ -3,7 +3,8 @@ import type { RouterConfig } from "@nuxt/schema";
 export default <RouterConfig>{
   scrollBehavior(to, from, savedPosition) {
     return {
-      top: 0
+      top: 0,
+      behavior: 'smooth',
     };
   }
 };
