3a10cb216d0c0a69a947b148c8311362a9cc30b3
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2024-02-22 21:28:04 +0100

    add image in main page

--- components/sections/TheHero.vue
+++ components/sections/TheHero.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="flex flex-col gap-8">
+  <div class="flex flex-col gap-5">
+    <div class="flex justify-center">
+      <img
+        :src="profileImg"
+        alt=""
+        class="w-72 h-auto md:w-96 rounded-full " />
+    </div>
     <MainTitle><i className="text-sm sm:text-lg md:text-2xl lg:text-3xl">joy</i>Full Stack Developer</MainTitle>
     <MainParagraph>
       I'm Fabrizio Silvestri, a Full Stack Developer and Tech Leader. I specialised my skils in Web Development with NodeJS, NoSQL databases and RESTful APIs. I am also a fan of Javascript frameworks and I like relying on Docker and AWS Cloud. (I have some experience in Python too!)
@@ -12,3 +18,7 @@
     </MainParagraph>
   </div>
 </template>
+
+<script setup lang="ts">
+import profileImg from "../../assets/imgs/profile.jpg";
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full">
     <div
-        class="max-w-screen-lg w-full mt-16 mb-20 px-8 mx-auto pb-24 sm:mt-28 sm:px-16 slg:px-24 lg:mt-48 lg:mt-58"
+        class="max-w-screen-lg w-full mt-8 mb-20 px-8 mx-auto pb-24 sm:mt-16 sm:px-16 slg:px-24 lg:mt-24 lg:mt-58"
     >
       <slot/>
     </div>
