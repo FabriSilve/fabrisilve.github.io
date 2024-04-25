25843f2a262dbde5981aa0af04f9d3117de8cfe6
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2024-04-25 19:53:34 +0200

    Update review sections to show only highlighed reviews

--- components/sections/Reviews.vue
+++ components/sections/Reviews.vue
@@ -1,39 +1,44 @@
 <template>
   <div>
-    <NuxtLayout name="section">
-      <SectionTitle>Words from Others</SectionTitle>
-      <p>Here's what others have to say about me.</p>
-      <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2 lg:gap-8 mt-8">
-        <ul class="space-y-8">
-          <ReviewItem
-            v-for="review in reviewsCol1"
-            :key="review.id"
-            :review="review.review"
-            :name="review.name"
-            :link="review.link"
-            :role="review.role"
-          />
-        </ul>
-        <ul class="hidden space-y-8 sm:block">
-          <ReviewItem
-            v-for="review in reviewsCol2"
-            :key="review.id"
-            :review="review.review"
-            :name="review.name"
-            :link="review.link"
-            :role="review.role"
-          />
-        </ul>
-      </div>
-    </NuxtLayout>
+    <div class="mb-3">
+      <NuxtLayout name="section">
+        <SectionTitle>Words from Others</SectionTitle>
+        <p>Here's what others have to say about me.</p>
+        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2 lg:gap-8 mt-8">
+          <ul class="space-y-8">
+            <ReviewItem
+              v-for="review in reviewsCol1"
+              :key="review.id"
+              :review="review.review"
+              :name="review.name"
+              :link="review.link"
+              :role="review.role"
+            />
+          </ul>
+          <ul class="hidden space-y-8 sm:block">
+            <ReviewItem
+              v-for="review in reviewsCol2"
+              :key="review.id"
+              :review="review.review"
+              :name="review.name"
+              :link="review.link"
+              :role="review.role"
+            />
+          </ul>
+        </div>
+      </NuxtLayout>
+    </div>
+    <MainParagraph>
+      <InlineLink to="reviews">Read more...</InlineLink>
+    </MainParagraph>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useReviewStore } from "~/store/reviewStore";
 
-const { getAllReviews } = useReviewStore();
+const { getBestReviews } = useReviewStore();
 
-const reviewsCol1 = getAllReviews.slice(0, Math.ceil(getAllReviews.length / 2));
-const reviewsCol2 = getAllReviews.slice(Math.ceil(getAllReviews.length / 2));
+const reviewsCol1 = getBestReviews.slice(0, Math.ceil(getBestReviews.length / 2));
+const reviewsCol2 = getBestReviews.slice(Math.ceil(getBestReviews.length / 2));
 </script>
