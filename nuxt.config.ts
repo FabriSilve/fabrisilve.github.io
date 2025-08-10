93bc5c15c44e19ac66e877c6212148242ba202e1
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2025-08-10 16:28:25 +0200

    cleanup configs files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@pinia/nuxt',
-        'nuxt-gtag',
-    ],
-    tailwindcss: {
-        cssPath: '~/assets/css/tailwind.css',
-        configPath: '~/tailwind.config.js',
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "~/tailwind.config.js",
+  },
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
     },
-    components: [{
-        path: '~/components', pathPrefix: false,
-    }],
-    content: {},
-    target: 'static',
-    router: {
-        base: '/'
-    },
-    gtag: {
-        id: 'G-ZVJQWJZ9BT',
-        initCommands: [
-            // Setup up consent mode
-            ['consent', 'default', {
-                ad_user_data: 'granted',
-                ad_personalization: 'granted',
-                ad_storage: 'granted',
-                analytics_storage: 'granted',
-                wait_for_update: 500,
-            }]
-        ]
-    }
-})
+  ],
+  content: {},
+  target: "static",
+  router: {
+    base: "/",
+  },
+});
